75d33d22d61a48e9cc320d43e8f4b34b1e791304
guilhermedrud <guilhermelima@dcc.ufmg.br>
2021-12-10 22:59:48 -0300

    deleted test files for subprocess

--- run_subprocess.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-f = open("input_test","r")
-cp = subprocess.run(["python3","main.py"],stdin=f, universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-print(cp)
-f.close()
